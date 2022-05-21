882a6b870f96d4611eaeb3d5fbb2024f55efbe17
sswastik02 <sswastik02@gmail.com>
2022-05-21 15:15:36 +0530

    Implemented Create Courses Endpoint
    Created courses creation (TODO: Implement Check if request.user == employer)

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.Course),
-    path('courses/getcourses',views.ListCoursesView.as_view(), name = 'getcourses')
+    path('courses/getcourses',views.ListCoursesView.as_view(), name = 'getcourses'),
+    path('courses/createcourse',views.CreateCoursesView.as_view(), name='createcourse')
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
-import imp
-from django.shortcuts import render
-from httplib2 import Response
-
 from rest_framework.generics import ListAPIView,CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_201_CREATED
+)
+from rest_framework.response import Response
 
 from accounts.models import Course
 from .serilaizers import CourseSerializer
@@ -14,6 +16,25 @@ class ListCoursesView(ListAPIView) :
 
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated]
+
+class CreateCoursesView(CreateAPIView):
+    queryset = Course.objects.all()
+
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request,*args,**kwargs):
+        user = request.user
+        if not True :
+            return Response({'error':'Only Employers can create courses'},status=HTTP_400_BAD_REQUEST)
+        serializer = CourseSerializer(data = request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=HTTP_201_CREATED)
+        return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
+
+
+
 
 
 
